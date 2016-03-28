6a487663d2fc6bbf1148e654fcaa77cc8871b6c7
AlexFine <alexkfine2@gmail.com>
2016-03-28 15:06:33 -0700

    Go die in a hole Camera.DestinationType.FILE_URL

--- QuillV2/www/js/controllers.js
+++ QuillV2/www/js/controllers.js
@@ -112,82 +112,27 @@ angular.module('starter.controllers', ['ion-gallery', 'ngCordova'])
     }
 
 
-
     //Select photo testing here
     // 1
-    $scope.images = [];
-
-    $scope.addImage = function () {
-        // 2
-        var options = {
-            destinationType: Camera.DestinationType.FILE_URI,
-            sourceType: Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
-            allowEdit: false,
-            encodingType: Camera.EncodingType.JPEG,
-            popoverOptions: CameraPopoverOptions,
-        };
-
-        // 3
-        $cordovaCamera.getPicture(options).then(function (imageData) {
-
-            // 4
-            onImageSuccess(imageData);
-
-            function onImageSuccess(fileURI) {
-                createFileEntry(fileURI);
-            }
-
-            function createFileEntry(fileURI) {
-                window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
-            }
-
-            // 5
-            function copyFile(fileEntry) {
-                var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
-                var newName = makeid() + name;
-
-                window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (fileSystem2) {
-                        fileEntry.copyTo(
-                            fileSystem2,
-                            newName,
-                            onCopySuccess,
-                            fail
-                        );
-                    },
-                    fail);
-            }
-
-            // 6
-            function onCopySuccess(entry) {
-                $scope.$apply(function () {
-                    $scope.images.push(entry.nativeURL);
-                });
-            }
-
-            function fail(error) {
-                console.log("fail: " + error.code);
-            }
-
-            function makeid() {
-                var text = "";
-                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-                for (var i = 0; i < 5; i++) {
-                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    $scope.choosePhoto = function () {
+                  var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.FILE_URL,
+                    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+   
+                    $cordovaCamera.getPicture(options).then(function (imageData) {
+                        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                    }, function (err) {
+                        // An error occured. Show a message to the user
+                    });
                 }
-                return text;
-            }
-
-        }, function (err) {
-            console.log(err);
-        });
-    }
-
-    $scope.urlForImage = function (imageName) {
-        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
-        var trueOrigin = cordova.file.dataDirectory + name;
-        return trueOrigin;
-    }
 })
 
 
