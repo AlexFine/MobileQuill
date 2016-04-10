e74868dd622bec9d0a9a58334a0e0d8ccb82ff5f
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-09 17:22:41 -0700

    memer

--- QuillV2/www/js/controllers.js
+++ QuillV2/www/js/controllers.js
@@ -18,14 +18,6 @@ angular.module('starter.controllers', ['ion-gallery', 'ngCordova'])
             }
         };
 
-
-
-
-
-
-
-
-
         $scope.checkLogged = function () {
 
             //Check if logged in here
@@ -498,23 +490,174 @@ angular.module('starter.controllers', ['ion-gallery', 'ngCordova'])
          //$scope.getmemes();
 
 
-         var memes = [
-            "http://25.media.tumblr.com/20463acf0cd7032c1047b03526bc80c4/tumblr_mm6typKnQB1qeak1oo1_500.gif",
-            "https://40.media.tumblr.com/358994cd528efde9d75e2088deeec8f4/tumblr_ne9stytIE81tpri36o1_500.jpg",
-            "http://a.fod4.com/misc/Creed%20taliban.gif",
-            "http://www.relatably.com/m/img/office-appropriate-memes/the-office-meme-jim.jpg",
-            "http://4.bp.blogspot.com/-4sEseI_hyC4/VRujMvF8ptI/AAAAAAAAD4A/oyTvQbvktr8/s1600/inside%2Bjokes.jpg",
-            "http://2.bp.blogspot.com/-MmEzOgZi2XQ/UYc94KwI7mI/AAAAAAAAB9c/rrpJt9gw4e0/s1600/MICHAELSFLAWS.jpg",
-            "http://3.bp.blogspot.com/-m7kV0Qf2ZMM/UYU8Pu4Ft3I/AAAAAAAAB88/yPrk29Hv87s/s1600/superstitious.jpg",
-            "https://s-media-cache-ak0.pinimg.com/736x/d2/1c/51/d21c517cd10e209038c42a66661251b4.jpg",
-            "http://memesvault.com/wp-content/uploads/Happy-Friday-Office-Meme-10.jpg",
-            "http://memesvault.com/wp-content/uploads/Funny-Meme-8.jpg",
-             "http://i3.kym-cdn.com/photos/images/newsfeed/000/611/250/de9.gif",
-             "http://cdn2.crushable.com/wp-content/uploads/2012/10/Screen-Shot-2012-10-05-at-10.49.48-AM.png",
-             "http://big.assets.huffingtonpost.com/ronswansononbacon4-17.400x226.gif",
-             "http://memesvault.com/wp-content/uploads/Funny-Pictures-With-Captions-About-Women-14.jpg",
-             "http://www.twitquotes.com/uploads/1/162.jpg"
-        ]
+         // $http.get("https://api.github.com/repos/kushaltirumala/memestorage/contents/memelist.json", {headers:{'Accept': 'application/json'}}).then(function(resp){
+
+         // }, function(err){
+
+         // })
+         
+    var Base64 = {
+
+
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+
+    encode: function(input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+        }
+
+        return output;
+    },
+
+
+    decode: function(input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+
+        }
+
+        output = Base64._utf8_decode(output);
+
+        return output;
+
+    },
+
+    _utf8_encode: function(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utftext;
+    },
+
+    _utf8_decode: function(utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while (i < utftext.length) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        }
+
+        return string;
+    }
+
+}      
+          var memes =[];
+          var response;
+         $http.get("https://api.github.com/repos/kushaltirumala/memestorage/contents/memelist.json", {headers:{'Accept': 'application/json'}}).then(function(resp){
+            response=JSON.parse(Base64.decode(resp.data.content));
+            for(var i = 0; i < response.links.length; i++) {
+              memes.push(response.links[i]);
+            }
+            $scope.json = memes;
+         }, function(err){
+            console.log(JSON.stringify(err));
+         })
+
+
+         //memes by kushal tirumala
+        //  var memes = [
+        //     "http://25.media.tumblr.com/20463acf0cd7032c1047b03526bc80c4/tumblr_mm6typKnQB1qeak1oo1_500.gif",
+        //     "https://40.media.tumblr.com/358994cd528efde9d75e2088deeec8f4/tumblr_ne9stytIE81tpri36o1_500.jpg",
+        //     "http://a.fod4.com/misc/Creed%20taliban.gif",
+        //     "http://www.relatably.com/m/img/office-appropriate-memes/the-office-meme-jim.jpg",
+        //     "http://4.bp.blogspot.com/-4sEseI_hyC4/VRujMvF8ptI/AAAAAAAAD4A/oyTvQbvktr8/s1600/inside%2Bjokes.jpg",
+        //     "http://2.bp.blogspot.com/-MmEzOgZi2XQ/UYc94KwI7mI/AAAAAAAAB9c/rrpJt9gw4e0/s1600/MICHAELSFLAWS.jpg",
+        //     "http://3.bp.blogspot.com/-m7kV0Qf2ZMM/UYU8Pu4Ft3I/AAAAAAAAB88/yPrk29Hv87s/s1600/superstitious.jpg",
+        //     "https://s-media-cache-ak0.pinimg.com/736x/d2/1c/51/d21c517cd10e209038c42a66661251b4.jpg",
+        //     "http://memesvault.com/wp-content/uploads/Happy-Friday-Office-Meme-10.jpg",
+        //     "http://memesvault.com/wp-content/uploads/Funny-Meme-8.jpg",
+        //      "http://i3.kym-cdn.com/photos/images/newsfeed/000/611/250/de9.gif",
+        //      "http://cdn2.crushable.com/wp-content/uploads/2012/10/Screen-Shot-2012-10-05-at-10.49.48-AM.png",
+        //      "http://big.assets.huffingtonpost.com/ronswansononbacon4-17.400x226.gif",
+        //      "http://memesvault.com/wp-content/uploads/Funny-Pictures-With-Captions-About-Women-14.jpg",
+        //      "http://www.twitquotes.com/uploads/1/162.jpg"
+        // ]
     //code goes here that will be run every 5 seconds.
              var meme = memes[Math.floor(Math.random()*memes.length)];
 
